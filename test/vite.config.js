59a031e9a575f9e994923349295975b7c1698e49
Seo Hyuneun <hyuneun@skuniv.ac.kr>
2024-07-17 17:47:20 +0900

    fix

--- test/vite.config.js
+++ test/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pushup-counter-test/"
+  base: "/pushup-counter-test/",
 })
